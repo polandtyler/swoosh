d795793f7ac6c2538e533bd149e4db0b06bb8c39
Tyler Poland <poland.tyler@gmail.com>
2018-08-31 11:30:47 -0600

    Add intent for Skills view

--- app/src/main/java/com/tylerpoland/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/tylerpoland/swoosh/WelcomeActivity.kt
@@ -1,11 +1,11 @@
 package com.tylerpoland.swoosh
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+//import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
