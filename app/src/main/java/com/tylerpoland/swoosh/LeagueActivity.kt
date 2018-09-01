434d6a4655a16ff1105f06669e11eb7000dfb468
Tyler Poland <poland.tyler@gmail.com>
2018-09-01 11:52:11 -0600

    passing data between activities + toasts

--- app/src/main/java/com/tylerpoland/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/tylerpoland/swoosh/LeagueActivity.kt
@@ -3,8 +3,12 @@ package com.tylerpoland.swoosh
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    private var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,7 +16,30 @@ class LeagueActivity : BaseActivity() {
     }
 
     fun leagueNextTapped(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onMensTapped(view: View) {
+        selectedLeague = "mens"
+        womensButton.isChecked = false
+        coEdButton.isChecked = false
+    }
+
+    fun onWomensTapped(view: View) {
+        selectedLeague = "womens"
+        mensButton.isChecked = false
+        coEdButton.isChecked = false
+    }
+
+    fun onCoEdTapped(view: View) {
+        selectedLeague = "coed"
+        mensButton.isChecked = false
+        womensButton.isChecked = false
     }
 }
